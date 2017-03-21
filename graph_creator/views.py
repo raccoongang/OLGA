31f5fef297b26a0266aa88151658d1a415224320
Shico-UA <s_shishka@ukr.net>
2017-03-21 12:42:26 +0200

    Changed IP settings to be capable to accept data with local server from the eDX server inside vagrant.

--- graph_creator/views.py
+++ graph_creator/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -12,10 +14,10 @@ class IndexView(View):
         return render(request, 'graph_creator/index.html', {'students_quantity': students_quantity})
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class ReceiveData(View):
-
     def post(self, request, *args, **kwargs):
-        received_data = int(self.request.POST.get('json'))
+        received_data = int(self.request.POST.get('number_of_students'))
         obj, created = DataStorage.objects.get_or_create(pk=1)
         obj.students_quantity = received_data
         obj.save()
