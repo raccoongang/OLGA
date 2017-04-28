0f5fa85da06b0ad01cbd6c55363c07e31dea90cf
Dmytryi <dmytryi.striletskyi@gmail.com>
2017-04-28 12:47:47 +0300

    Change field`s name

--- graph_creator/models.py
+++ graph_creator/models.py
@@ -12,10 +12,10 @@ class DataStorage(models.Model):
     Model that stores data received from the edx-platform.
     """
 
+    active_students_amount = models.IntegerField(default=0)
     courses_amount = models.IntegerField(default=0)
     latitude = models.FloatField(default=0, blank=True)
     longitude = models.FloatField(default=0, blank=True)
     platform_name = models.CharField(max_length=255, null=True, blank=True)
     platform_url = models.URLField(null=True, blank=True)
     secret_token = models.CharField(max_length=255, null=True)
-    students_amount = models.IntegerField(default=0)
