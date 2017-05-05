1e1b1fcb47a351f4cc98b809a2d244b7e75c4a2a
Dmytryi <dmytryi.striletskyi@gmail.com>
2017-05-05 15:01:15 +0300

    Add students per country logic, refactor (levels, data control flow)

--- graph_creator/views.py
+++ graph_creator/views.py
@@ -1,4 +1,5 @@
 import uuid
+import json
 
 import requests
 
@@ -16,13 +17,13 @@ from .models import DataStorage
 class IndexView(View):
     """
     Displays information on a world map.
-    
+
     Retrieve information about edx-platform from DB, serialize it into JSON format and
     pass serialized data to the template. The template displays a map of the world with the
     edx-platform marker on it.
-    
+
     `edx_data_as_json` is a `DataStorage` containing all information about edx-platform.
-    
+
     Returns http response and passes `edx_data_as_json` data as a context variable `edx_data`
     to the `index.html` template.
     """
@@ -30,7 +31,7 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         """
         Retrieve information about edx-platform from DB and serialize it into JSON.
-        
+
         `edx_data_as_json` is a `DataStorage` containing all information about edx-platform.
         """
 
@@ -42,60 +43,99 @@ class IndexView(View):
 class ReceiveData(View):
     """
     Receives and processes data from the remote edx-platform.
-    
+
     If the platform has already registered, a normal data exchange happens.
     Otherwise generates a secret token, (registers)saves it to DB with the edx-platform's incoming URL
-    and sends newly generated token to the edx-platform for further 
+    and sends newly generated token to the edx-platform for further
     data interchange abilities with the server.
     """
+
+    @staticmethod
+    def students_per_country(active_students_amount, students_per_country_to_decode):
+        """
+        Method calculates amount of students per country.
+
+        Arguments:
+            active_students_amount (int): Count of activate students.
+            students_per_country_to_decode (unicode): Unicode oriented to decode in list of dictionaries.
+
+        Returns:
+            students_per_country (str) : List of dictionaries, where one of them is country-count accordance.
+        """
+
+        students_per_country = json.loads(students_per_country_to_decode)
+
+        students_per_country[0]['count'] = active_students_amount - sum(
+            [country['count'] for country in students_per_country]
+        )
+
+        students_per_country = json.dumps(students_per_country)
+
+        return students_per_country
 
     def post(self, request, *args, **kwargs):
         """
         Receive information from the edx-platform and processes it.
-        
+
         If the first time gets information from edx-platform, generates a secret token and
         sends it back to the edx-platform for further data exchange abilities, otherwise
         updates data in the DB with the new incoming information from the edx-platform.
-        
+
         `secret_token` when generates is a uuid.UUID in string format.
         `reverse_token` is a requests. Sends the secret token to edx-platform.
-        
+
         Returns http response redirecting to the main page.
         """
 
-        # TODO: make serializer validation for received data
         received_data = self.request.POST
-        active_students_amount = received_data.get('active_students_amount')
-        courses_amount = received_data.get('courses_amount')
-        latitude = received_data.get('latitude')
-        longitude = received_data.get('longitude')
-        platform_name = received_data.get('platform_name')
-        platform_url = received_data.get('platform_url')
+        level = received_data.get('level')
         secret_token = received_data.get('secret_token')
+        active_students_amount = int(received_data.get('active_students_amount'))
 
-        # TODO: make creation of data, not updating exist
         if secret_token:
-            DataStorage.objects.filter(secret_token=str(secret_token)).update(
-                active_students_amount=int(active_students_amount),
-                courses_amount=int(courses_amount),
-                latitude=float(latitude),
-                longitude=float(longitude),
-                platform_url=platform_url,
-                platform_name=platform_name,
-            )
+            if level == 'enthusiast':
+
+                instance_data = {
+                    'active_students_amount': active_students_amount,
+                    'courses_amount': int(received_data.get('courses_amount')),
+                    'latitude': float(received_data.get('latitude')),
+                    'level': level,
+                    'longitude': float(received_data.get('longitude')),
+                    'platform_name': received_data.get('platform_name'),
+                    'platform_url': received_data.get('platform_url'),
+                    'secret_token': secret_token,
+                    'students_per_country': self.students_per_country(
+                        active_students_amount, received_data.get('students_per_country')
+                    )
+                }
+
+            else:  # level is paranoid
+                instance_data = {
+                    'active_students_amount': active_students_amount,
+                    'courses_amount': int(received_data.get('courses_amount')),
+                    'level': level,
+                    'secret_token': secret_token
+                }
+
+            DataStorage.objects.create(**instance_data)
             return HttpResponse(status=200)
 
         else:
             secret_token = uuid.uuid4().hex
-            DataStorage.objects.create(
-                active_students_amount=int(active_students_amount),
-                courses_amount=int(courses_amount),
-                latitude=float(latitude),
-                longitude=float(longitude),
-                platform_name=platform_name,
-                platform_url=platform_url,
-                secret_token=secret_token
-            )
+
+            instance_data = {
+                'active_students_amount': active_students_amount,
+                'courses_amount': int(received_data.get('courses_amount')),
+                'latitude': float(received_data.get('latitude')),
+                'level': level,
+                'longitude': float(received_data.get('longitude')),
+                'platform_name': received_data.get('platform_name'),
+                'platform_url': received_data.get('platform_url'),
+                'secret_token': secret_token,
+                'students_per_country': self.students_per_country(
+                        active_students_amount, received_data.get('students_per_country')
+                )
+            }
 
             if settings.DEBUG:
                 requests.post(
@@ -106,6 +146,6 @@ class ReceiveData(View):
 
             else:
                 requests.post(
-                    str(platform_url) + '/acceptor_data/', data={"secret_token": secret_token}
+                    str(instance_data['platform_url']) + '/acceptor_data/', data={"secret_token": secret_token}
                 )
                 return HttpResponse(status=201)
