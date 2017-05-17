903d5d74250036d85bcb7b0c42287b958a58514d
Alexander Kryklia <kryklia@gmail.com>
2017-05-17 23:15:17 +0300

    Add extended fixtures.

---
+++ graph_creator/utils.py
@@ -0,0 +1,115 @@
+"""
+Utilities
+
+How to: from graph_creator folder run:
+python utils.py
+python ../manage.py loaddata test_fixture.json
+and reload page.
+
+"""
+import datetime
+import json
+from random import random
+
+class LoadTestModel(object):
+	"""
+	Model that describes test data
+
+	end_date is always today.
+	"""
+
+	# clusters of instances
+	clusters = { 
+		"small": {
+			'courses': (1, 10),
+		 	'students': (1, 1000),
+		 	'quantity': (1,100),
+		},
+		"middle": {
+	 		'courses': (10, 50), # min / max 
+		 	'students': (500, 2500),
+	 		'quantity': 10,
+		},
+		"large": {
+	 		'courses': (50, 150),
+		 	'students': (2500, 10000),
+	 		'quantity': (1,100),
+		},
+	}
+
+	# history of data
+	days = 600
+
+	# probability to send data per day per instance
+	send_data = 0.95
+
+	def probability_to_add_course():
+		return probability(0.5 * (cls.clusters['middle']['courses'][0] + cls.clusters['middle']['courses'][1])/cls.days)
+
+	def probability_to_add_student():
+		return probability(0.5 * (cls.clusters['middle']['students'][0] + cls.clusters['middle']['students'][1])/cls.days)
+
+
+def probability(x):
+	return random() < x
+	
+
+def generate_model():
+
+	cl = LoadTestModel()
+	# for middle cluster
+
+	instance_list = []
+	students = courses = 0
+	number_of_instances = cl.clusters['middle']['quantity']
+
+	instance_dict = {}
+	students_dict = {}
+
+
+	for instance_num in range(number_of_instances):
+		for day in range(cl.days):
+			if probability(cl.send_data):
+				dt = (datetime.datetime.today() - datetime.timedelta(days=cl.days - day)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+				if cl.probability_to_add_course:
+					instance_dict[instance_num] = instance_dict.get(instance_num, 0) + 1
+					students_dict[instance_num] = students_dict.get(instance_num, 0) + 25
+
+				courses = instance_dict.get(instance_num, 0)
+
+				if cl.probability_to_add_student:
+					students_dict[instance_num] = students_dict.get(instance_num, 0) + 0
+
+				students = students_dict.get(instance_num, 0)
+
+
+			  	instance = {
+				    "model": "graph_creator.datastorage",
+				    "pk": instance_num * cl.days + day,
+				    "fields": {
+						"active_students_amount_day": students,
+						"active_students_amount_week": 1000,
+						"active_students_amount_month": 1000,
+						"courses_amount": courses,
+						"data_update": dt,
+						"latitude": 1.0,
+						"longitude": 1.0,
+						"platform_name": "test",
+						"platform_url": "test",
+						"secret_token": "platform" + " " + str(instance_num),
+						"statistics_level": "1",
+						"students_per_country": "{\"RU\": 2632, \"CA\": 18543, \"UA\": 2011, \"null\": 1}"
+				    }
+				}
+				 	
+			 	instance_list.append(instance)
+
+	return json.dumps(instance_list)
+
+if __name__ == '__main__':
+	
+	data = generate_model()
+	f = open('test_fixture.json', 'w')
+	f.write(data)
+	f.close()
