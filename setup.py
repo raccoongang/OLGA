01223b934a25be6c043ce0adf580eae0c79795d0
Dmytryi Striletskyi <dmytryi.striletskyi@gmail.com>
2017-07-31 17:10:02 +0300

    Add setup.py (#49)

--- setup.py
+++ setup.py
@@ -5,17 +5,22 @@ Setup for acceptor.
 from setuptools import setup
 
 VERSION = "0.1.0"
-DESCRIPTION = "Acceptor server which takes specific data from edx-platform and visualizes the data on the charts"
+DESCRIPTION = "OLGA server which takes specific data from edx-platform and visualizes the data on the charts"
 
 
 setup(
-    name="acceptor",
+    name="OLGA",
     version=VERSION,
     description=DESCRIPTION,
     license='AGPL v3',
     install_requires=[
-            'Django>=1.11',
-            'pycountry>=17.1.8',
-            'requests>=2.13.0',
+        'ddt>=1.1.1',
+        'Django>=1.11',
+        'gunicorn>=19.7.1',
+        'factory-boy==2.8.1',
+        'mock==2.0.0',
+        'psycopg2==2.7.1',
+        'pycountry>=17.1.8',
+        'requests>=2.13.0',
     ],
 )
