37201e1a62244ed2b1b7fa92564a72240fc4abf3
Arsen Turchanikov <arsen.tur@gmail.com>
2019-02-22 18:33:40 +0200

    Refactor code upgrade django to 2.1.7

--- web/olga/charts/static/charts/worldmap.js
+++ web/olga/charts/static/charts/worldmap.js
@@ -30,6 +30,7 @@ function compose_dataset(datamap_data) {
     datamap = new Datamap({
         element: document.getElementById('datamap-container'),
         projection: 'mercator', // Big world map.
+
         // Countries don't listed in dataset will be painted with this color.
         fills: { defaultFill: '#F5F5F5' },
         data: dataset,
@@ -59,8 +60,6 @@ function compose_dataset(datamap_data) {
             }
         }
     });
-
-    datamap;
 
     /**
      * Scaling map.
